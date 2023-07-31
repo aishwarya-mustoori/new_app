9f10b86ed38c069637692022825096e63ec16f69
Aishwarya Mustoori <59821571+aishwarya-mustoori@users.noreply.github.com>
2023-07-30 22:13:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Resume from './resume.js'
+import Skills from './skills';
+import Education from './education';
+import Navigation from './navigation';
+import Experience from './experience';
+
+import { useState } from 'react';
+import { createContext } from 'react';
+import ContactBar from './contactBar';
+import Projects from './project';
+const LayoutContext = createContext();
+
 
 function App() {
+  const [isColumn, setIsColumn] = useState(true);
+  const styleLayoutColumn = {
+    flexDirection: isColumn ? 'column' : 'column',
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <LayoutContext.Provider value={[isColumn, setIsColumn]}>
+
+      <div className='App bg-dark'>
+        <Navigation></Navigation>
+
+        <section id="about">
+          <ContactBar></ContactBar>
+
+          <Resume />
+        </section>
+
+
+        <section id="skills">
+          <Skills />
+        </section>
+        <section id="experience">
+          <Experience />
+        </section>
+        <section id="education">
+          <Education />
+        </section>
+        <section id="projects">
+          <Projects>
+          </Projects>
+        </section>
+      </div>
+
+
+    </LayoutContext.Provider>
+
   );
 }
 
